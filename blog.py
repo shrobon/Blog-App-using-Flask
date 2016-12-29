6f564fb3289edbdc3e268315a4908a7167b67785
shrobon <shrobon.biswas@gmail.com>
2016-12-28 23:47:31 -0700

    Posts showing after logging in

--- blog.py
+++ blog.py
@@ -59,9 +59,13 @@ def login_required(test):
 
 
 @app.route('/main')
-@login_required # This decorator has been added
+@login_required # This decorator has been added to stop unauthenticated login
 def main():
-	return render_template('main.html')
+	g.db = connect_db()
+	cur = g.db.execute('select * from posts')
+	posts = [dict(title=row[0],post=row[1])for row in cur.fetchall()]
+	g.db.close()
+	return render_template('main.html', posts = posts)
 
 
 
